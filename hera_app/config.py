f31a4e01f40c3291fe3df81c913a36413e125831
Lisa Wagner <wagnerl@mskcc.org>
2019-03-12 12:18:55 -0400

    new mysql dialect

--- hera_app/config.py
+++ hera_app/config.py
@@ -2,7 +2,7 @@ SECRET_KEY = ***
 WTF_CSRF_SECRET_KEY = ***
 AUTH_LDAP_URL = 'ldaps://ldapha.mskcc.root.mskcc.org/'
 
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://$USER:$PW@&HOST:$PORT/$DB"
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql:://$USER:$PW@&HOST:$PORT/$DB"
 
 # explicitly disabling according to SQLAlchemy's docs
 SQLALCHEMY_TRACK_MODIFICATIONS = False
