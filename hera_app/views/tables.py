851414ef323cdd62f5b351b98d42e506b1c4408a
Lisa Wagner <wagnerl@mskcc.org>
2019-04-01 14:59:59 -0400

    switched to handsontable

--- hera_app/views/tables.py
+++ hera_app/views/tables.py
@@ -3,6 +3,7 @@ from flask_table import Table, Col
 from flask import render_template, flash, redirect, url_for, jsonify, session, Blueprint
 import json
 from sqlalchemy import create_engine
+import sqlalchemy as db
 from hera_app import app
 
 
@@ -34,60 +35,33 @@ class ItemTable(Table):
         return url_for('tables.index', sort=col_key, direction=direction)
 
 
-
-
-
-
-
 @tables.route('/tables')
 @login_required
 def index():
+
     #  move to sth more persistent once columns are more finalized
-    sample_id_db_columns = engine.execute('SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA="samplestatus"  AND TABLE_NAME="sample";')
-    sample_id_db_data = engine.execute('SELECT * FROM sample')
-
-    sample_id_columns_dict = {}
-    sample_id_columns = []
-    for row in sample_id_db_columns:
-        sample_id_columns_dict['name'] = row[0]
-        sample_id_columns_dict['type'] = 'text'
-        sample_id_columns_dict['align'] = 'left'
-        sample_id_columns.append(sample_id_columns_dict.copy())
-
-    # sample_columns = engine.execute('SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA="samplestatus"  AND TABLE_NAME="sample";')
+    sample_id_db_columns = engine.execute(
+        'SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA="samplestatus"  AND TABLE_NAME="sample";'
+    )
+    sample_id_db_data = engine.execute('SELECT * FROM sample').fetchall()
+    columnHeaders = generateColumnHeaders(sample_id_db_columns)
+    data = generateData(sample_id_db_data, columnHeaders)
+    return render_template('tables.html', columnHeaders=columnHeaders, data=data)
 
 
+def generateColumnHeaders(dbColumns):
+    columnHeaders = [col for col, in dbColumns]
+    return columnHeaders
+
+
+def generateData(dbData, columnHeaders):
     sample_id_data_dict = {}
     sample_id_data = []
-    for row in sample_id_db_data:
-        sample_id_data_dict['id'] = row[0]
-        sample_id_data_dict['altId'] = row[1]
-        sample_id_data_dict['correctedCmoId'] = row[2]
-        sample_id_data_dict['investigatorEmail'] = row[3]
-        sample_id_data_dict['userSampleId'] = row[4]
+    for row in dbData:
+        for i, col in enumerate(columnHeaders):
+            sample_id_data_dict[col] = '' if row[i] is None else row[i]
         sample_id_data.append(sample_id_data_dict.copy())
-
-
-    print(sample_id_columns)
-
-
-    # sample_result = engine.execute('SELECT * FROM sample')
-    # print(sample_result)
-    # items = User.query.all() + User.query.all() + User.query.all()
-    # table = ItemTable(items)
-    # print(jsonify(json_list = items))
-    # print(jsonify([i.serialize for i in items]).sample_id_columns)
-    # print(tables)
-    # return render_template('tables.html', table=table)
-    # print(sample_ids)
-    return render_template(
-        'tables.html',
-        items=items,
-        samples=items,
-        sample_states=sample_states,
-        sample_id_data=sample_id_data,
-        sample_id_columns = sample_id_columns
-    )
+    return sample_id_data
 
 
 @tables.route('/sample_timeline')
